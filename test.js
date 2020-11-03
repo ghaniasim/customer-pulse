e539f3d1d5e4da0118c21ae0fa9e8f517335c504
ghaniasim <ghaniasimm@gmail.com>
2020-11-03 21:03:07 +0200

    first test

--- test.js
+++ test.js
@@ -1,29 +1,7 @@
-import chai from "chai";
-import fetch from "node-fetch";
+const assert = require("assert");
 
-describe("Test API call", () => {
-  it("should return status 200", async () => {
-    //if fetch url is incorrect it will return 404 == false assert
-    chai.assert.equal(
-      await fetch("https://meri.digitraffic.fi/api/v1/locations/latest")
-        .then((res) => {
-          return res.status;
-        })
-        .catch((res) => console.log(res)),
-      200
-    );
-  });
-
-  it("should return status 200, but this time with incorrect url", async () => {
-    //if fetch url is incorrect it will return 404 == false assert
-    //this time with incorrect url
-    chai.assert.equal(
-      await fetch("https://meri.digitraffic.fi/api/v1/locations/latestttt")
-        .then((res) => {
-          return res.status;
-        })
-        .catch((res) => console.log(res)),
-      200
-    );
+describe("Test", () => {
+  it("should return ok", () => {
+    assert.strictEqual("ok", "ok");
   });
 });
