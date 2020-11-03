ef705c9fe022604b9c00d2f78a31b160e1015e99
ghaniasim <ghaniasimm@gmail.com>
2020-11-03 23:50:31 +0200

    fail test

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
-const assert = require("assert");
+import chai from "chai";
+import fetch from "node-fetch";
 
-describe("Test", () => {
-  it("should return ok", () => {
-    assert.strictEqual("ok", "ok");
+describe("Test API call", () => {
+  it("This should not return status 200", async () => {
+    //Url is incorrect it will return 404
+    chai.assert.equal(
+      await fetch("https://meri.digitraffic.fi/api/v1/locations/latestttt")
+        .then((res) => {
+          return res.status;
+        })
+        .catch((res) => console.log(res)),
+      200
+    );
   });
 });
