75e2f75c079dcd39e1a229e8da122a98aff3b5ac
Kashif <kashifqamar111@gmail.com>
2020-12-01 04:49:30 +0200

    half way added questions, survey and post data with fetch

--- components/create.js
+++ components/create.js
@@ -7,46 +7,116 @@ import {
   TextInput,
 } from "react-native";
 
-const Create = () => {
+const Create = ({ navigation }) => {
   // creating the object for the survey array
 
-  const [teacherName, setTeacherName] = useState("");
+  const [teacherName, setTeacherName] = useState(
+    navigation.state.params.teacherName
+  );
+
   const [surveyName, setSurveyName] = useState("");
   const [questions, setQuestions] = useState([]);
 
   //Now creating the object for questions array
 
-  const [questionNumber, setQuestionNumber] = useState("");
-  const [questionType, setQuestionType] = useState("");
-  const [questionText, setQuestionText] = useState("");
+  const surveyObject = {
+    teacherName: teacherName,
+    surveyName: surveyName,
+    questions: [
+      {
+        answers: [
+          {
+            answer: "yes",
+          },
+        ],
 
-  const updateInputVal = (val, prop) => {
-    if (prop === "surveyName") {
-      setSurveyName(val);
-    }
-    if (prop === "questionText") {
-      setQuestionText(val);
-    }
-    setQuestionNumber(1);
+        questionNumber: "1",
+        questionType: "YESNO",
+        questionText: "Are you working on your project?",
+      },
+    ],
   };
-
-  //const surveyItem = { id: idNum, name: title, q1: question };
-  //console.log("surveyItem", surveyItem);
 
   return (
     <View>
+      <Text style={styles.createSurvey}>Create Your Survey</Text>
       <TextInput
+        style={styles.inputStyle}
         placeholder="Name of survey"
         value={surveyName}
-        onChangeText={(val) => updateInputVal(val, "surveyName")}
+        onChangeText={setSurveyName}
       />
-      <TextInput
-        placeholder="Write your question"
-        value={questionText}
-        onChangeText={(val) => updateInputVal(val, "questionText")}
-      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          navigation.navigate("Question");
+        }}
+      >
+        <Text>Add Questions</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.buttonBottom}>
+        <Text>Create Survey</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: "3%",
+
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#ade8f4",
+    height: 50,
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: "#000000",
+    marginTop: "5%",
+    marginLeft: "20%",
+  },
+  buttonBottom: {
+    backgroundColor: "#ade8f4",
+    height: 50,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: "#000000",
+    marginTop: "100%",
+  },
+  inputStyle: {
+    width: "80%",
+    marginBottom: "5%",
+    alignSelf: "center",
+    borderColor: "#ccc",
+    borderBottomWidth: 1,
+    marginTop: "5%",
+  },
+  chooseText: {
+    textAlign: "center",
+    color: "#023e8a",
+    fontWeight: "bold",
+    fontSize: 20,
+    width: "100%",
+    marginBottom: 20,
+  },
+  createSurvey: {
+    textAlign: "center",
+    color: "#023e8a",
+    fontWeight: "bold",
+    fontSize: 30,
+    width: "100%",
+    marginBottom: 20,
+    marginTop: "7%",
+  },
+});
 export default Create;
