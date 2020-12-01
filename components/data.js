ba1aac8618bbd38bfa419396c544ff1f940a5e2e
Kashif <kashifqamar111@gmail.com>
2020-12-01 12:37:50 +0200

    create a survey and respond to available surveys

---
+++ components/data.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+
+const Data = ({ navigation }) => {
+  const [data, setData] = useState();
+  const [loaded, setLoaded] = useState(false);
+
+  async function getData() {
+    const res = await fetch(`http://192.168.1.223:8001/feedbacks`);
+    res
+      .json()
+      .then((res) => {
+        setData(res);
+        setLoaded(true);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }
+
+  useEffect(() => {
+    getData();
+  }, [data]);
+  return (
+    <View>
+      <FlatList
+        style={{ marginTop: "15%", margin: "5%" }}
+        keyExtractor={(survey) => survey.id}
+        data={data}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                navigation.navigate("SurveyObject", {
+                  survey: item,
+                });
+              }}
+            >
+              <Text style={styles.surveyTitle}>
+                {item.surveyName} by {item.teacherName}
+              </Text>
+            </TouchableOpacity>
+          );
+        }}
+      ></FlatList>
+    </View>
+  );
+};
+
+export default Data;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: "15%",
+    paddingTop: "7%",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  button: {
+    shadowColor: "rgba(0,0,0, .4)", // IOS
+    shadowOffset: { height: 1, width: 1 }, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 1, //IOS
+    backgroundColor: "#ade8f4",
+    elevation: 2, // Android
+    height: 50,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: "#000000",
+    marginTop: 10,
+  },
+});
---
+++ components/object.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  FlatList,
+  Button,
+} from "react-native";
+
+const SurveyObject = ({ route, navigation }) => {
+  const [survey, setSurvey] = useState(navigation.state.params.survey);
+
+  const questions = survey.questions;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.surveyName}>{survey.surveyName}</Text>
+      <Text style={styles.teacherName}>Created by: {survey.teacherName}</Text>
+      <Text style={styles.numberOfQuestions}>
+        Total number of questions: {questions.length}
+      </Text>
+      <FlatList
+        keyExtractor={(question) => question._id}
+        data={questions}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  navigation.navigate("Answers", {
+                    questionObject: item,
+                  });
+                }}
+              >
+                <Text>
+                  {item.questionNumber} :{item.questionText}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.viewButton}>
+                <Text>Reply</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      ></FlatList>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: "3%",
+
+    justifyContent: "center",
+  },
+  viewButton: {
+    backgroundColor: "#ade8f4",
+    height: 50,
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: "#000000",
+    marginTop: "5%",
+  },
+  button: {
+    backgroundColor: "#ade8f4",
+    height: 50,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: "#000000",
+    marginTop: 10,
+  },
+  surveyName: {
+    textAlign: "center",
+    color: "#023e8a",
+    fontWeight: "bold",
+    fontSize: 30,
+    width: "100%",
+    marginBottom: 20,
+  },
+  teacherName: {
+    textAlign: "center",
+    color: "#023e8a",
+    fontWeight: "bold",
+    fontSize: 20,
+    width: "100%",
+    marginBottom: 20,
+  },
+  numberOfQuestions: {
+    textAlign: "center",
+    color: "#023e8a",
+    fontWeight: "bold",
+    fontSize: 15,
+    width: "100%",
+    marginBottom: 20,
+  },
+});
+
+export default SurveyObject;
