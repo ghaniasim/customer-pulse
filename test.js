c58bc9ed6868732bd7bbc51753ce7e799892aa81
ghaniasim <ghaniasimm@gmail.com>
2020-12-08 23:44:09 +0200

    CI tests added

--- test.js
+++ test.js
@@ -1,7 +1,28 @@
 const assert = require("assert");
+const chai = require("chai");
+const fetch = require("node-fetch");
 
 describe("Test", () => {
-  it("Should not return ok", () => {
+  it("Should return ok", () => {
     assert.strictEqual("ok", "ok");
+  });
+});
+
+describe("#indexOf()", () => {
+  it("should return -1 when the value is not present", function () {
+    assert.strictEqual([1, 2, 3].indexOf(4), -1);
+  });
+});
+
+describe("Test API call", () => {
+  it("should return status 200", async () => {
+    chai.assert.equal(
+      await fetch("https://customer-pulse-backend.herokuapp.com")
+        .then((res) => {
+          return res.status;
+        })
+        .catch((res) => console.log(res)),
+      200
+    );
   });
 });
