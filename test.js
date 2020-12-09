2b4d4e16f08269e21a940c77346034b5d27a0b17
ghaniasim <ghaniasimm@gmail.com>
2020-12-09 04:12:20 +0200

    refactor CI tests

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
 const assert = require("assert");
-const request = require("supertest")(
-  "https://customer-pulse-backend.herokuapp.com"
-);
-const expect = require("chai").expect;
 
 describe("Test", () => {
   it("Should return ok", () => {
@@ -13,13 +9,5 @@ describe("Test", () => {
 describe("#indexOf()", () => {
   it("should return -1 when the value is not present", function () {
     assert.strictEqual([1, 2, 3].indexOf(4), -1);
-  });
-});
-
-describe("Test API call", () => {
-  it("should return status 200", async () => {
-    const response = await request.get("/");
-
-    expect(response.status).to.eql(200);
   });
 });
